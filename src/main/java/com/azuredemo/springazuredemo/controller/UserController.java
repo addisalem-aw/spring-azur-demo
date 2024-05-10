267005cbb4fb5f447bd6a04c064597100a618be4
Addisalem Wordofa <addaber@gmail.com>
2024-05-10 16:00:17 -0400

    #1

--- src/main/java/com/azuredemo/springazuredemo/controller/UserController.java
+++ src/main/java/com/azuredemo/springazuredemo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.azuredemo.springazuredemo.entity.User;
 import com.azuredemo.springazuredemo.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private UserService userService;
     @GetMapping
-    public ResponseEntity<String> getUser(User user){
+    public ResponseEntity<String> getUser(@RequestBody User user){
         String u=userService.getUser(user);
         return ResponseEntity.ok(u);
-
-
     }
 }
